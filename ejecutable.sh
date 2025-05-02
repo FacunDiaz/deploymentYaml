5b1a6b1fdaab2bd84ee81968e7ab1508af496de4
Facu123 <facundomartin05@gmail.com>
2025-05-02 20:31:52 +0000

    script funcional

--- ejecutable.sh
+++ ejecutable.sh
@@ -11,8 +11,8 @@ set -e
 #USO:
 #	./ejecutable.sh
 
-REPO_MANIFIESTOS="https://github.com/<nombre_usuario>/deploymentYaml.git"
-REPO_STATIC="https://github.com/<nombre_usuario>/static-website.git" 
+REPO_MANIFIESTOS="https://github.com/FacunDiaz/deploymentYaml.git"
+REPO_STATIC="https://github.com/FacunDiaz/static-website.git" 
 MINIKUBE_PROFILE="deploy-web-site"
 
 function verificar_herramientas(){
@@ -43,25 +43,26 @@ fi
 cd minik-static-web
 
 #Clonamos los repos dentro de la carpeta
-git clone $REPO_MANIFIESTOS || {echo "Fallo al clonar el repositorio de manifiestos";exit 1}
-git clone $REPO_STATIC || {echo "Fallo al clonar el repositorio del contenido estático";exit 1}
+git clone $REPO_MANIFIESTOS || {echo "Fallo al clonar el repositorio de manifiestos";exit 1 }
+git clone $REPO_STATIC || {echo "Fallo al clonar el repositorio del contenido estático";exit 1 }
 
 #Crear perfil de minikube con el cluster
-minikube start --profile=$MINIKUBE_PROFILE||{echo "Error iniciando MINIKUBE";exit 1}
+minikube start --profile=$MINIKUBE_PROFILE||{echo "Error iniciando MINIKUBE";exit 1 }
 minikube profile $MINIKUBE_PROFILE
 minikube addons enable metrics-server --profile=$MINIKUBE_PROFILE
 
 #Nos posicionames en la carpeta donde están los manifiestos y los aplicamos 
-cd minik-static-web/deploymentYaml/manifiestos
+cd "$HOME"/minik-static-web/deploymentYaml/manifiestos
 kubectl apply -f namespace.yaml
 kubectl apply -f pvc/pv.yaml
 kubectl apply -f pvc/pvc.yaml
 kubectl apply -f deployment/nginx-deployment.yaml
 kubectl apply -f service/nginx-service.yaml
-cd $HOME
-#Montamos la carpeta con el contenido estático en el volumen persistente (comando en segundo plano) 
-nohup minikube mount "$HOME"/minik-static-web/static-website:/mnt/data/web-content &> /dev/null & || {echo "Fallo al hacer montaje";exit 1}
-#Reiniciamos el deployment para el nginx ahora pueda ver los archivos que aparecieron en el volumen persistente
-kubectl rollout restart deployment nginx -n web-site || {echo "el restart del deployment fallo";exit 1}
 
+#Montamos la carpeta con el contenido estático en el volumen persistente (comando en segundo plano) 
+nohup minikube mount "$HOME"/minik-static-web/static-website:/mnt/data/web-content &> /dev/null &
+sleep 2
+#Reiniciamos el deployment para el nginx ahora pueda ver los archivos que aparecieron en el volumen persistente
+kubectl rollout restart deployment nginx -n web-site
+sleep 2
 minikube service nginx-service -n web-site --url
