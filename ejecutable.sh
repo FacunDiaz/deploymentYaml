3aa8df80388b77b19af138dfa2d116c5f8578f77
Facu123 <facundomartin05@gmail.com>
2025-05-02 19:22:30 +0000

    ejecutable casi final

--- ejecutable.sh
+++ ejecutable.sh
@@ -13,6 +13,7 @@
 REPO_MANIFIESTOS="https://github.com/<nombre_usuario>/deploymentYaml.git"
 REPO_STATIC="https://github.com/<nombre_usuario>/static-website.git"
 
+
 cd $HOME
 mkdir minikube-static-web 
 cd minikube-static-web 
@@ -33,3 +34,22 @@ function verificar_herramientas(){
                 echo "Git no está instalado"
         fi
 }
+verificar_herramientas 
+#Crear perfil de minikube con el cluster
+minikube start --profile=deploy-web-site
+minikube profile deploy-web-site
+minikube addons enable metrics-server --profile=deploy-web-site
+
+#Nos posicionames en la carpeta donde están los manifiestos y los aplicamos 
+cd minikube-static-web/deploymentYaml/manifiestos
+kubectl apply -f namespace.yaml
+kubectl apply -f pvc/pv.yaml
+kubectl apply -f pvc/pvc.yaml
+kubectl apply -f deployment/nginx-deployment.yaml
+kubectl apply -f service/nginx-service.yaml
+cd $HOME
+minikube mount minikube-static-web/static-website:/mnt/data/web-content
+#Reiniciamos el deployment para el nginx ahora pueda ver los archivos que aparecieron en el volumen persistente
+kubectl rollout restart deployment nginx -n web-site
+
+minikube service nginx-service -n web-site --url
