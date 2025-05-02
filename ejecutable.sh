4b90d7b8a3e5e871e51f453100adbab9d632acac
Facu123 <facundomartin05@gmail.com>
2025-05-02 20:12:25 +0000

    ejecutable actualizado

--- ejecutable.sh
+++ ejecutable.sh
@@ -1,7 +1,8 @@
 #!/bin/bash 
 #
-
-#-------------------------------------------
+#Si algun comando falla, se va a parar la ejecución del script
+set -e 
+#-----------------------------------------
 #Scrit de despliegue entorno de desarrollo en Minikube 
 #Autor: Facundo Diaz
 #Fecha: 02/05/25
@@ -11,45 +12,56 @@
 #	./ejecutable.sh
 
 REPO_MANIFIESTOS="https://github.com/<nombre_usuario>/deploymentYaml.git"
-REPO_STATIC="https://github.com/<nombre_usuario>/static-website.git"
-
-
-cd $HOME
-mkdir minikube-static-web 
-cd minikube-static-web 
-
-#verificar la instalación de git 
-
-git clone $REPO_MANIFIESTOS
-git clone $REPO_STATIC
+REPO_STATIC="https://github.com/<nombre_usuario>/static-website.git" 
+MINIKUBE_PROFILE="deploy-web-site"
 
 function verificar_herramientas(){
 	if ! command -v git &> /dev/null ; then 
 		echo "Git no está instalado"
+		exit 1
 	fi
 	if ! command -v kubectl &> /dev/null ; then
-                echo "Git no está instalado"
+                echo "Kubeclt no está instalado"
+		exit 1
         fi
 	if ! command -v minikube &> /dev/null ; then
-                echo "Git no está instalado"
+                echo "Minikube no está instalado"
+		exit 1
         fi
 }
+#Ejecutamos la función para verificar las dependencias
 verificar_herramientas 
+
+#Verificar que no haya otra carpeta con el mismo nombre 
+cd $HOME || exit 1  
+if [ ! -d minik-static-web ]; then
+	mkdir minik-static-web
+else 
+	echo "el directorio minik-static-web ya existe, no se puede seguir con el despliegue"
+	exit 1
+fi
+cd minik-static-web
+
+#Clonamos los repos dentro de la carpeta
+git clone $REPO_MANIFIESTOS || {echo "Fallo al clonar el repositorio de manifiestos";exit 1}
+git clone $REPO_STATIC || {echo "Fallo al clonar el repositorio del contenido estático";exit 1}
+
 #Crear perfil de minikube con el cluster
-minikube start --profile=deploy-web-site
-minikube profile deploy-web-site
-minikube addons enable metrics-server --profile=deploy-web-site
+minikube start --profile=$MINIKUBE_PROFILE||{echo "Error iniciando MINIKUBE";exit 1}
+minikube profile $MINIKUBE_PROFILE
+minikube addons enable metrics-server --profile=$MINIKUBE_PROFILE
 
 #Nos posicionames en la carpeta donde están los manifiestos y los aplicamos 
-cd minikube-static-web/deploymentYaml/manifiestos
+cd minik-static-web/deploymentYaml/manifiestos
 kubectl apply -f namespace.yaml
 kubectl apply -f pvc/pv.yaml
 kubectl apply -f pvc/pvc.yaml
 kubectl apply -f deployment/nginx-deployment.yaml
 kubectl apply -f service/nginx-service.yaml
 cd $HOME
-minikube mount minikube-static-web/static-website:/mnt/data/web-content
+#Montamos la carpeta con el contenido estático en el volumen persistente (comando en segundo plano) 
+nohup minikube mount "$HOME"/minik-static-web/static-website:/mnt/data/web-content &> /dev/null & || {echo "Fallo al hacer montaje";exit 1}
 #Reiniciamos el deployment para el nginx ahora pueda ver los archivos que aparecieron en el volumen persistente
-kubectl rollout restart deployment nginx -n web-site
+kubectl rollout restart deployment nginx -n web-site || {echo "el restart del deployment fallo";exit 1}
 
 minikube service nginx-service -n web-site --url
